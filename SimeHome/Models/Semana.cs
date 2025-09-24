a559cee0d99fe235ca574ba20fb4a8c1e490c23a
Raphael567 <raphaelcanuto464@gmail.com>
2025-09-23 22:49:40 -0300

    Feat: Datas e Footer

---
+++ SimeHome/Models/Semana.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimeHome.Models
+{
+    public class Semana
+    {
+        public string Dia { get; set; }
+        public string Nome { get; set; }
+    }
+}
--- SimeHome/ViewModels/HomeViewModel.cs
+++ SimeHome/ViewModels/HomeViewModel.cs
@@ -8,6 +8,9 @@ namespace SimeHome.ViewModels
     {
         public ObservableCollection<string> Filtros { get; set; }
         public ObservableCollection<Sala> Salas { get; set; }
+        public ObservableCollection<int> Dias { get; set; }
+        public ObservableCollection<Semana> Semana { get; set; }
+        public ObservableCollection<string> FooterIcons { get; set; }
 
         public ICommand IsPressed { get; }
 
@@ -28,7 +31,32 @@ namespace SimeHome.ViewModels
                 new Sala { Num = "4", NumChamados = "01" }
             };
 
+            Semana = new ObservableCollection<Semana>()
+            {
+                new Semana { Dia = "29", Nome = "seg" },
+                new Semana { Dia = "30", Nome = "ter" },
+                new Semana { Dia = "1", Nome = "qua" },
+                new Semana { Dia = "2", Nome = "qui" },
+                new Semana { Dia = "3", Nome = "sex" },
+                new Semana { Dia = "4", Nome = "sab" },
+            };
+
             IsPressed = new Command<Sala>(isPressed);
+
+            Dias = new ObservableCollection<int>(
+                Enumerable.Range(1, 30)
+            );
+
+            var nomesBase = new string[]
+            {
+                "home",
+                "pendentes",
+                "criar_chamado",
+                "concluidas",
+                "criar",
+            };
+
+            FooterIcons = new ObservableCollection<string>(nomesBase.Select(nome => $"{nome}.png"));
         }
 
         public void isPressed(Sala sala)
